3208353eddee72db87c5ff06d97b55455fd9b477
张昌硕 <3312280576@qq.com>
2021-02-18 16:55:15 +0800

    2021.2.18 afternoon

--- import.py
+++ import.py
@@ -9,4 +9,6 @@ for glyph in oldFont:
     	newGlyph=newFont.createChar(oldFont[glyph].unicode,oldFont[glyph].glyphname)
     	newGlyph.importOutlines("./fz_svg/"+str(oldFont[glyph].unicode)+"_"+oldFont[glyph].glyphname+".svg")
     	newGlyph.width=oldFont[glyph].width
+newFont.familyname="DaiJinXiang"
+newFont.fullname="DaiJinXiang"
 newFont.generate("out.ttf")
